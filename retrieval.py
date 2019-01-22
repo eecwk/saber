499e3f75b9ed914ee19573f680446c62c7a23ac0
Christopher Kelly <eecwk@leeds.ac.uk>
2019-01-22 17:01:07 +0000

    Added function for generating profile plots.

--- retrieval.py
+++ retrieval.py
@@ -1,6 +1,8 @@
 import netCDF4
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.gridspec as gridspec
+import math
 from matplotlib import colors
 
 deg = unichr(176)
@@ -31,10 +33,22 @@ def make_retrieval_arrays(tracer, set_year, set_day):
             tracer_bin[j,k] = tracer_scans_mean
     return tracer_bin      
 
+def calc_cos_factor(tracer_bin, lowlat, highlat):
+    tracer_weighted = np.zeros(16)    
+    for j in range (0,16):    
+        sig_cos_x = 0
+        sig_cos = 0
+        for k in range (lowlat, highlat):
+            sig_cos_x = sig_cos_x + (math.cos(math.radians(lats[k])) * tracer_bin[j][k])
+            sig_cos = sig_cos + math.cos(math.radians(lats[k]))         
+            if  k == (highlat - 1):
+                tracer_weighted[j] = sig_cos_x / sig_cos
+    return tracer_weighted
+
 def pressure_plot_2d(tracer_bin, name, units):
     if name == 'ozone':
         diffs = [1.e+7, 1.75e+7, 2.5e+7, 3.25e+7, 4.e+7, 4.75e+7, 5.5e+7, 6.25e+7, 7.e+7, 7.75e+7, 8.5e+7, 9.25e+7, 1.e+8, 1.75e+8, 2.5e+8, 3.25e+8, 4.e+8, 4.75e+8, 5.5e+8, 6.25e+8, 7.e+8, 7.75e+8, 8.5e+8, 9.25e+8, 1.e+9]
-    x, y = np.meshgrid(lats_bins, pressure)
+    x, y = np.meshgrid(lats_bin, pressure)
     plt.contourf(x[:,:], y[:,:], tracer_bin, diffs, norm=colors.LogNorm(), cmap=plt.get_cmap('viridis'))
     cbar = plt.colorbar()
     cbar.ax.tick_params(labelsize=12)
@@ -51,7 +65,7 @@ def pressure_plot_2d(tracer_bin, name, units):
 def altitude_plot_2d(tracer_bin, name, units):
     if name == 'ozone':
         diffs = [1.e+7, 1.75e+7, 2.5e+7, 3.25e+7, 4.e+7, 4.75e+7, 5.5e+7, 6.25e+7, 7.e+7, 7.75e+7, 8.5e+7, 9.25e+7, 1.e+8, 1.75e+8, 2.5e+8, 3.25e+8, 4.e+8, 4.75e+8, 5.5e+8, 6.25e+8, 7.e+8, 7.75e+8, 8.5e+8, 9.25e+8, 1.e+9]
-    x, y = np.meshgrid(lats_bins, pressure)
+    x, y = np.meshgrid(lats_bin, pressure)
     y = alt_bin
     plt.contourf(x[:,:], y[:,:], tracer_bin, diffs, norm=colors.LogNorm(), cmap=plt.get_cmap('viridis'))
     cbar = plt.colorbar()
@@ -63,14 +77,64 @@ def altitude_plot_2d(tracer_bin, name, units):
     plt.ylabel('Altitude [km]', fontsize=12)
     plt.title('%s, DOY=%s' %(set_year, set_day), fontsize=14)  
 
+def altitude_plot_1d(name, tracer_weighted, alt_weighted, lowlat, highlat, units, plot_no):
+    lowlat_no = int((lowlat * 5) - 90)
+    highlat_no = int((highlat * 5) - 90)
+    if plot_no > 5:
+        plot_no = plot_no - 6
+    plt.subplot(gs1[plot_no])
+    plt.title('%s%s to %s%s' %(lowlat_no, deg, highlat_no, deg), fontsize=14)
+    x = tracer_weighted[::-1]
+    y = alt_weighted[::-1]
+    plt.plot(x, y, color='k', label='saber')
+    if plot_no == 0:
+        plt.ylabel('Altitude [km]', fontsize=12)
+        plt.tick_params(labelbottom='off')
+    if plot_no == 1:
+        plt.tick_params(labelleft='off')
+        plt.tick_params(labelbottom='off')
+    if plot_no == 2:
+        plt.tick_params(labelleft='off')
+        plt.tick_params(labelbottom='off')
+    if plot_no == 3:
+        plt.xlabel('%s [%s]' %(name, units), fontsize=12)
+        plt.ylabel('Altitude [km]', fontsize=12)
+    if plot_no == 4:
+        plt.xlabel('%s [%s]' %(name, units), fontsize=12)
+        plt.tick_params(labelleft='off')
+    if plot_no == 5:
+        plt.xlabel('%s [%s]' %(name, units), fontsize=12)
+        plt.tick_params(labelleft='off')
+    plt.ylim(77,100)
+    plt.xlim(0,7.e+8)
+    if plot_no == 2:
+        plt.legend(loc=1)
+    return
+
+def setup_altitude_plot_1d(tracer_bin, name, units):
+    plt.suptitle('%s, DOY=%s' %(set_year, set_day), fontsize=14)
+    for i in range(0,6):    
+        lowlat = i * 6
+        highlat = (i * 6) + 6
+        tracer_weighted = calc_cos_factor(tracer_bin, lowlat, highlat)
+        alt_weighted = calc_cos_factor(alt_bin, lowlat, highlat)
+        altitude_plot_1d(name, tracer_weighted, alt_weighted, lowlat, highlat, units, i)
+
 set_year = 2014
 set_day = 6
 
 o3_bin = make_retrieval_arrays(o3, set_year, set_day)
 alt_bin = make_retrieval_arrays(alt, set_year, set_day)
-lats_bins = np.arange(-87.5, 92.5, 5)
+lats_bin = np.arange(-87.5, 92.5, 5)
 
+# 1D plot
+fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(11,8))
+gs1 = gridspec.GridSpec(2, 3)
+gs1.update(wspace=0.1, hspace=0.1)
+setup_altitude_plot_1d(o3_bin, 'ozone', '$\mathregular{cm^{-3}}$')
+
+# 2D plot
 #pressure_plot_2d(o3_bin, 'ozone', '$\mathregular{cm^{-3}}$')
-altitude_plot_2d(o3_bin, 'ozone', '$\mathregular{cm^{-3}}$')
+#altitude_plot_2d(o3_bin, 'ozone', '$\mathregular{cm^{-3}}$')
 
 plt.show()
